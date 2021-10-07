1565f492354c4a8e28c5a709250dec0304c764c7
Peter Boers <peter.boers@surf.nl>
2021-10-07 14:31:27 +0200

    Change requires key

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup
 
 from orchestrator import __version__
 
-setup_variables = toml.load("pyproject.toml")
+setup_variables = toml.load("pyproject.toml")["tool"]["flit"]["metadata"]
 
 setup(
-    name=setup_variables["tool"]["dist-name"],
+    name=setup_variables["dist-name"],
     version=__version__,
-    classifiers=setup_variables["tool"]["classifiers"],
-    module=setup_variables["tool"]["module"],
-    author=setup_variables["tool"]["author"],
-    author_email=setup_variables["tool"]["author_email"],
-    packages=setup_variables["tool"]["dist-name"],
-    requires=setup_variables["tool"]["requires"],
-    description_file=setup_variables["tool"]["README.md"],
+    classifiers=setup_variables["classifiers"],
+    author=setup_variables["author"],
+    author_email=setup_variables["author-email"],
+    packages=[setup_variables["module"]],
+    install_requires=setup_variables["requires"],
+    description="The Orchestrator core",
+    long_description=setup_variables["description-file"],
 )
