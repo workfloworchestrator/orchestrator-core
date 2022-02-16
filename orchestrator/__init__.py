4111b7620f13139d66d7e41a4d0c107db9edce25
René Dohmen <acidjunk@gmail.com>
2022-02-16 14:37:46 +0100

    Bumped version to 0.3.5

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
