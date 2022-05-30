de241ff3b908828e82cb374e2074998285104935
Peter Boers <peter.boers@surf.nl>
2022-05-30 11:35:20 +0300

    Handle non existant subscription in domain-model endpoint correctly (#143)

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "0.4.1-rc4"
+__version__ = "0.4.1"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
