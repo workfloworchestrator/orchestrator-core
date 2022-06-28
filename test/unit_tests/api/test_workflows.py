b35709485f97988766bf2128b191bd1f715cb5c3
Peter Boers <peter.boers@surf.nl>
2022-06-28 09:19:39 +0200

    Implement Etag and If-None-Match headers

--- test/unit_tests/api/test_workflows.py
+++ test/unit_tests/api/test_workflows.py
@@ -1,7 +1,15 @@
 from http import HTTPStatus
+from os import getenv
+
+import pytest
+from redis import Redis
 
 from orchestrator.db import WorkflowTable
+from orchestrator.domain.base import SubscriptionModel
+from orchestrator.settings import app_settings
 from orchestrator.targets import Target
+from orchestrator.utils.functional import orig
+from orchestrator.workflows.steps import cache_domain_models
 
 PRODUCT_ID = "fb28e465-87fd-4d23-9c75-ed036529e416"
 
@@ -36,3 +44,20 @@ def test_get_all_with_product_tags(test_client):
 
     assert HTTPStatus.OK == response.status_code
     assert len(response.json()) == WorkflowTable.query.count()
+
+
+@pytest.mark.skipif(
+    not getenv("AIOCACHE_DISABLE", 0) == 0, reason="AIOCACHE must be enabled for this test to do anything"
+)
+def test_push_subscriptions_to_cache_step(generic_subscription_1):
+    push_subscription = orig(cache_domain_models)
+    push_subscription("Aribtrary_name", SubscriptionModel.from_subscription(generic_subscription_1))
+
+    cache = Redis(host=app_settings.CACHE_HOST, port=app_settings.CACHE_PORT)
+    assert cache.get(f"domain:{generic_subscription_1}") is None
+
+    app_settings.CACHE_DOMAIN_MODELS = True
+    push_subscription("Aribtrary_name", SubscriptionModel.from_subscription(generic_subscription_1))
+    result = cache.get(f"domain:{generic_subscription_1}")
+    assert result is not None
+    app_settings.CACHE_DOMAIN_MODELS = False
