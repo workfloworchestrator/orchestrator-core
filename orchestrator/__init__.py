d867f430ac303729e981cdb7a865ffab350954bc
Peter Boers <peter.boers@surf.nl>
2022-06-29 09:26:50 +0200

    Bumpversion

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "0.4.4-rc2"
+__version__ = "0.4.4-rc3"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
