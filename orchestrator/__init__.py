cefda05db854b4662ddcfd52aa0e47994a139230
René Dohmen <acidjunk@gmail.com>
2023-02-01 14:02:05 +0100

    Bumped version to 0.5.3-rc1

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "0.5.2"
+__version__ = "0.5.3-rc1"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
