87406b8536a0046aac4ae7a432b6cde765139981
Mark Moes <mark_moes@live.nl>
2023-02-09 10:26:43 +0100

    Fix docker image tagging (#210)
    * Add docker/metadata-action's default tags which are sadly overwritten instead of extended
    
    * Use docker/metadata-action's recommended way to extract version label
    
    * Bump version to 0.5.3-rc3
    
    * Actually fix docker tagging + document local testing

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "0.5.3-rc2"
+__version__ = "0.5.3-rc3"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
