80ec4d413ca5e6474fd41d37edf2f8bf97d45e4a
Peter Boers <peter.boers@surf.nl>
2023-06-27 13:40:14 +0200

    Stay at rc4

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "1.1.1rc5"
+__version__ = "1.1.1rc4"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
