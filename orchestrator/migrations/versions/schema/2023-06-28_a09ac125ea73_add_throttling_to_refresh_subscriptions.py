38124b385fc4cf591951ff03d5c7ab064743bd09
Daniel Marjenburgh <daniel.marjenburgh@surf.nl>
2023-07-04 09:07:00 +0200

    Optimize subscriptions_search_view re-indexing (#296)

---
+++ orchestrator/migrations/versions/schema/2023-06-28_a09ac125ea73_add_throttling_to_refresh_subscriptions.py
@@ -0,0 +1,28 @@
+"""Add throttling to refresh_subscriptions_view trigger.
+
+Revision ID: a09ac125ea73
+Revises: b1970225392d
+Create Date: 2023-06-28 15:33:36.248121
+
+"""
+from pathlib import Path
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "a09ac125ea73"
+down_revision = "b1970225392d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    conn = op.get_bind()
+
+    revision_file_path = Path(__file__)
+    with open(revision_file_path.with_suffix(".sql")) as f:
+        conn.execute(f.read())
+
+
+def downgrade() -> None:
+    pass
