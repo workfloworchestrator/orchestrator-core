48be26533c0ae2aeadd262cebed9e346160bc142
Maurits Rijk <maurits.rijk@surf.nl>
2023-08-22 15:37:20 +0200

    Trial and error

--- orchestrator/api/api_v1/endpoints/subscriptions.py
+++ orchestrator/api/api_v1/endpoints/subscriptions.py
@@ -226,9 +226,9 @@ def subscriptions_filterable(
     "/workflows/{subscription_id}", response_model=SubscriptionWorkflowListsSchema, response_model_exclude_none=True
 )
 def subscription_workflows_by_id(subscription_id: UUID) -> Dict[str, List[Dict[str, Union[List[Any], str]]]]:
-    subscription = SubscriptionTable.query.options(joinedload(SubscriptionTable.product), joinedload("product.workflows")).get(
-        subscription_id
-    )
+    subscription = SubscriptionTable.query.options(
+        joinedload(SubscriptionTable.product), joinedload("product.workflows")
+    ).get(subscription_id)
     if not subscription:
         raise_status(HTTPStatus.NOT_FOUND)
 
