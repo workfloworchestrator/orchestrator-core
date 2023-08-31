9a0ceafa032cf33ec1520f257ac915387cea7998
tjeerddie <tjeerdverschragen@hotmail.com>
2023-08-31 13:55:41 +0200

    Update orchestrator/api/helpers.py

--- orchestrator/api/helpers.py
+++ orchestrator/api/helpers.py
@@ -237,7 +237,7 @@ def enrich_process(p: ProcessTable) -> _ProcessListItem:
         created_by=p.created_by,
         failed_reason=p.failed_reason,
         last_modified_at=p.last_modified_at,
-        process_id=p.process_id,
+        pid=p.process_id,
         started_at=p.started_at.timestamp(),
         last_status=p.last_status,
         last_step=p.last_step,
