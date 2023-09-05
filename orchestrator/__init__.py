6d2869378e4adb6710d632df662c87681af162a0
Peter Boers <peter.boers@surf.nl>
2023-09-05 09:42:37 +0200

    1.2.3

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "1.2.3rc6"
+__version__ = "1.2.3"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
