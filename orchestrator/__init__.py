1873ea8e3e557485902d3f952fe336fe5263abf8
Peter Boers <peter.boers@surf.nl>
2023-09-20 10:25:42 +0200

    Bumpversion SQAlchemy

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "1.2.7"
+__version__ = "1.3.0"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
