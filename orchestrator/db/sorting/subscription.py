026009ebd7ea8f5b7bd149506176f7a4b1dac1a6
tjeerddie <tjeerdverschragen@hotmail.com>
2023-10-05 15:54:22 +0200

    Add missing sort fields in processes and subscriptions

--- orchestrator/db/sorting/subscription.py
+++ orchestrator/db/sorting/subscription.py
@@ -1,15 +1,36 @@
-from sqlalchemy.inspection import inspect
+from typing import Callable
 
-from orchestrator.db import SubscriptionTable
-from orchestrator.db.sorting.product import PRODUCT_SORT_FUNCTIONS_BY_COLUMN
-from orchestrator.db.sorting.sorting import generic_column_sort, generic_sort
+from sqlalchemy import Column
+from sqlalchemy.inspection import inspect
+from sqlalchemy.sql import expression
+
+from orchestrator.db import ProductTable, SubscriptionTable
+from orchestrator.db.database import SearchQuery
+from orchestrator.db.sorting.sorting import SortOrder, generic_column_sort, generic_sort
 from orchestrator.utils.helpers import to_camel
+
+
+def generic_subscription_relation_sort(field: Column) -> Callable[[SearchQuery, SortOrder], SearchQuery]:
+    def sort_function(query: SearchQuery, order: SortOrder) -> SearchQuery:
+        if order == SortOrder.DESC:
+            return query.order_by(expression.desc(field))
+        return query.order_by(expression.asc(field))
+
+    return sort_function
+
+
+SUBSCRIPTION_PRODUCT_SORT = {
+    to_camel(key if "product" in key else f"product_{key}"): generic_subscription_relation_sort(value)
+    for key, value in inspect(ProductTable).columns.items()
+}
 
 subscription_table_sort = {
     to_camel(key): generic_column_sort(value) for [key, value] in inspect(SubscriptionTable).columns.items()
 }
 
-SUBSCRIPTION_SORT_FUNCTIONS_BY_COLUMN = PRODUCT_SORT_FUNCTIONS_BY_COLUMN | subscription_table_sort
+SUBSCRIPTION_SORT_FUNCTIONS_BY_COLUMN = (
+    SUBSCRIPTION_PRODUCT_SORT | subscription_table_sort | {"tag": SUBSCRIPTION_PRODUCT_SORT["productTag"]}
+)
 
 subscription_sort_fields = list(SUBSCRIPTION_SORT_FUNCTIONS_BY_COLUMN.keys())
 sort_subscriptions = generic_sort(SUBSCRIPTION_SORT_FUNCTIONS_BY_COLUMN)
