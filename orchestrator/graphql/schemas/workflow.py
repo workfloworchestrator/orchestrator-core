7278817a91fda9b388b9522ba6ad805f8b2886ff
Maurits Rijk <maurits.rijk@surf.nl>
2023-10-18 16:37:23 +0200

    Add step resolver to workflow graphql

--- orchestrator/graphql/schemas/workflow.py
+++ orchestrator/graphql/schemas/workflow.py
@@ -1,8 +1,10 @@
-from typing import TYPE_CHECKING, Annotated, List
+from typing import TYPE_CHECKING, Annotated, List, Optional
 
 import strawberry
 
+from orchestrator.config.assignee import Assignee
 from orchestrator.schemas import StepSchema, WorkflowSchema
+from orchestrator.workflows import get_workflow
 
 if TYPE_CHECKING:
     from orchestrator.graphql.schemas.product import ProductType
@@ -10,7 +12,7 @@ if TYPE_CHECKING:
 
 @strawberry.experimental.pydantic.type(model=StepSchema, all_fields=True)
 class Step:
-    pass
+    assignee: Optional[Assignee]
 
 
 @strawberry.experimental.pydantic.type(model=WorkflowSchema, all_fields=True)
@@ -20,3 +22,7 @@ class Workflow:
         from orchestrator.graphql.schemas.product import ProductType
 
         return [ProductType.from_pydantic(product) for product in self._original_model.products]  # type: ignore
+
+    @strawberry.field(description="Return all steps for this workflow")
+    def steps(self) -> List[Step]:
+        return [Step(name=step.name, assignee=step.assignee) for step in get_workflow(self.name).steps]  # type: ignore
