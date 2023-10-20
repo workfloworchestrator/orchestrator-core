6bb6c9325ee4a26bc25613c8461a41b8693c8d42
Hans Trompert <hans.trompert@surf.nl>
2023-10-20 03:14:20 -0500

    Generator improvements (#392)
    - replace typer.echo() with proper log messages
    - exit when product configuration cannot be read
    - change type of all path variables from str to Path
    - add 'variable' (snake-cased product name) to config
    - make --config-file option required
    - honour DATABASE_URI and PYTHONPATH environment variables while generating migration
    - correct location of database migration files
    - fix the generation of translations
    - fix the generation of lazy workflow definitions
    - remove superflous whitespace from templates
    - fix migration template
    - import missing Optional in product template
    - add variable to product blocks config
    - add option to specify custom templates folder for generate workflow commando
    - extent Jinja filesystem loader search path with custom folder
    - create missing dunder init files
    - add product_types_module to workflow renderers
    - refactor create and modify workflow templates to leverage new custom
    - templating capabilities
    - replace update_ticket_step with addtional_steps
    - while failing to read product config raise exception instead of exit()
    - create migration head 'data' if it does not exist
    - add products for all fixed input combinations to migration
    - add all new products to subscription model registry
    - close workflow file before calling writer()
    - fix typos in subscription descriptions
    - use begin instead of StepList()
    - make input fields Optional if not required for that lifecycle state
    - make input fields ReadOnlyField's if not modifiable
    - fix modify workflow summary page
    - refactor terminate workflow template to include custom templates

--- orchestrator/workflows/utils.py
+++ orchestrator/workflows/utils.py
@@ -26,7 +26,7 @@ from orchestrator.targets import Target
 from orchestrator.types import State, SubscriptionLifecycle
 from orchestrator.utils.redis import caching_models_enabled
 from orchestrator.utils.state import form_inject_args
-from orchestrator.workflow import Step, StepList, Workflow, conditional, done, init, make_workflow, step
+from orchestrator.workflow import StepList, Workflow, begin, conditional, done, init, make_workflow, step
 from orchestrator.workflows.steps import (
     cache_domain_models,
     remove_domain_model_from_cache,
@@ -186,7 +186,7 @@ def create_workflow(
     description: str,
     initial_input_form: Optional[InputStepFunc] = None,
     status: SubscriptionLifecycle = SubscriptionLifecycle.ACTIVE,
-    update_ticket_step: Optional[Step] = None,
+    additional_steps: StepList = begin,
 ) -> Callable[[Callable[[], StepList]], Workflow]:
     """Transform an initial_input_form and a step list into a workflow with a target=Target.CREATE.
 
@@ -202,11 +202,10 @@ def create_workflow(
     create_initial_input_form_generator = wrap_create_initial_input_form(initial_input_form)
 
     def _create_workflow(f: Callable[[], StepList]) -> Workflow:
-        execute_ticket_step = conditional(lambda state: state.get("ticket_id", False))
         steplist = (
             init
             >> f()
-            >> execute_ticket_step(update_ticket_step)
+            >> additional_steps
             >> set_status(status)
             >> resync
             >> push_domain_models(cache_domain_models)
@@ -221,7 +220,7 @@ def create_workflow(
 def modify_workflow(
     description: str,
     initial_input_form: Optional[InputStepFunc] = None,
-    update_ticket_step: Optional[Step] = None,
+    additional_steps: StepList = begin,
 ) -> Callable[[Callable[[], StepList]], Workflow]:
     """Transform an initial_input_form and a step list into a workflow.
 
@@ -229,8 +228,8 @@ def modify_workflow(
 
     Example::
 
-        @modify_workflow("create service port") -> StepList:
-        def create_service_port():
+        @modify_workflow("modify service port") -> StepList:
+        def modify_service_port():
             do_something
             >> do_something_else
     """
@@ -238,14 +237,13 @@ def modify_workflow(
     wrapped_modify_initial_input_form_generator = wrap_modify_initial_input_form(initial_input_form)
 
     def _modify_workflow(f: Callable[[], StepList]) -> Workflow:
-        execute_ticket_step = conditional(lambda state: state.get("ticket_id", False))
         steplist = (
             init
             >> store_process_subscription(Target.MODIFY)
             >> push_domain_models(remove_domain_model_from_cache)
             >> unsync
             >> f()
-            >> execute_ticket_step(update_ticket_step)
+            >> additional_steps
             >> resync
             >> push_domain_models(cache_domain_models)
             >> done
@@ -259,7 +257,7 @@ def modify_workflow(
 def terminate_workflow(
     description: str,
     initial_input_form: Optional[InputStepFunc] = None,
-    update_ticket_step: Optional[Step] = None,
+    additional_steps: StepList = begin,
 ) -> Callable[[Callable[[], StepList]], Workflow]:
     """Transform an initial_input_form and a step list into a workflow.
 
@@ -267,8 +265,8 @@ def terminate_workflow(
 
     Example::
 
-        @terminate_workflow("create service port") -> StepList:
-        def create_service_port():
+        @terminate_workflow("terminate service port") -> StepList:
+        def terminate_service_port():
             do_something
             >> do_something_else
     """
@@ -276,14 +274,13 @@ def terminate_workflow(
     wrapped_terminate_initial_input_form_generator = wrap_modify_initial_input_form(initial_input_form)
 
     def _terminate_workflow(f: Callable[[], StepList]) -> Workflow:
-        execute_ticket_step = conditional(lambda state: state.get("ticket_id", False))
         steplist = (
             init
             >> store_process_subscription(Target.TERMINATE)
             >> push_domain_models(remove_domain_model_from_cache)
             >> unsync
             >> f()
-            >> execute_ticket_step(update_ticket_step)
+            >> additional_steps
             >> set_status(SubscriptionLifecycle.TERMINATED)
             >> resync
             >> push_domain_models(cache_domain_models)
