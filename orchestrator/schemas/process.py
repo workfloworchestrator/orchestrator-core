b2012bf70aa487465a077fadd8ba8983c2f8a8c6
Mark90 <mark_moes@live.nl>
2023-10-24 18:37:24 +0200

    Migrate Reporter to pydantic 2.x

--- orchestrator/schemas/process.py
+++ orchestrator/schemas/process.py
@@ -12,10 +12,10 @@
 # limitations under the License.
 
 from datetime import datetime
-from typing import Any, Dict, List, Optional
+from typing import Annotated, Any, Dict, List, Optional
 from uuid import UUID
 
-from pydantic import ConfigDict, ConstrainedStr
+from pydantic import ConfigDict, Field
 
 from orchestrator.config.assignee import Assignee
 from orchestrator.schemas.base import OrchestratorBaseModel
@@ -111,5 +111,4 @@ class ProcessStatusCounts(OrchestratorBaseModel):
     task_counts: Dict[ProcessStatus, int]
 
 
-class Reporter(ConstrainedStr):
-    max_length = 100
+Reporter = Annotated[str, Field(max_length=100)]
