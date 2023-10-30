85eb60d7ef05b48b69a3e4ae96ee01040923937b
Mark90 <mark_moes@live.nl>
2023-10-30 15:45:14 +0100

    Fix various deprecation warnings

--- orchestrator/graphql/resolvers/subscription.py
+++ orchestrator/graphql/resolvers/subscription.py
@@ -14,7 +14,7 @@
 from typing import Optional
 
 import structlog
-from pydantic.utils import to_lower_camel
+from pydantic.alias_generators import to_camel as to_lower_camel
 from sqlalchemy import func, select
 
 from orchestrator.db import ProductTable, SubscriptionTable, db
