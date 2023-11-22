ae4c95bc562caa225a45ff0236eda934fe63e677
Mark90 <mark_moes@live.nl>
2023-11-22 10:06:36 +0100

    Bump version to 2.0.0a0
    Signed-off-by: Mark90 <mark_moes@live.nl>

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "1.3.5"
+__version__ = "2.0.0a0"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
