c96d5ef633b0dc62fbe331cad01743e3a95e0223
Daniel Marjenburgh <daniel.marjenburgh@surf.nl>
2023-12-06 10:47:17 +0100

    Use own to_camel function. Pydantic version converts to CamelCase, not camelCase

--- orchestrator/db/filters/search_filters/__init__.py
+++ orchestrator/db/filters/search_filters/__init__.py
@@ -1,6 +1,5 @@
-from typing import Type, TypeVar
+from typing import Type
 
-from pydantic.utils import to_camel
 from sqlalchemy import inspect
 
 from orchestrator.db.database import BaseModel
@@ -8,12 +7,11 @@ from orchestrator.db.filters.search_filters.inferred_filter import inferred_filt
 
 __all__ = ["inferred_filter", "default_inferred_column_clauses", "node_to_str_val"]
 
+from orchestrator.utils.helpers import to_camel
 from orchestrator.utils.search_query import WhereCondGenerator
 
-T = TypeVar("T", bound=Type[BaseModel])
 
-
-def default_inferred_column_clauses(table: T) -> dict[str, WhereCondGenerator]:
+def default_inferred_column_clauses(table: Type[BaseModel]) -> dict[str, WhereCondGenerator]:
     return {
         k: inferred_filter(column)
         for key, column in getattr(inspect(table), "columns", {}).items()
