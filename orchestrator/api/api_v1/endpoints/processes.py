3cc488b7d5e85448882f2a7861ac0a994797b23c
Daniel Marjenburgh <daniel.marjenburgh@surf.nl>
2024-01-11 15:13:43 +0100

    Formatting

--- orchestrator/api/api_v1/endpoints/processes.py
+++ orchestrator/api/api_v1/endpoints/processes.py
@@ -266,9 +266,7 @@ def status_counts() -> ProcessStatusCounts:
 
     stmt = (
         select(ProcessTable)
-        .with_only_columns(
-            ProcessTable.is_task, ProcessTable.last_status, count(ProcessTable.last_status)
-        )
+        .with_only_columns(ProcessTable.is_task, ProcessTable.last_status, count(ProcessTable.last_status))
         .group_by(ProcessTable.is_task, ProcessTable.last_status)
     )
     rows = db.session.scalars(stmt)
