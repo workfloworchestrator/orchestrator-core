25b17498fbcb4f414c4357d15e39d7468cdb6631
Daniel Marjenburgh <daniel.marjenburgh@surf.nl>
2024-01-24 15:36:44 +0100

    Fix sorting for CompoundSelect statements
    Signed-off-by: Mark90 <mark_moes@live.nl>

--- orchestrator/db/sorting/product.py
+++ orchestrator/db/sorting/product.py
@@ -5,7 +5,7 @@ from orchestrator.db.sorting.sorting import generic_column_sort, generic_sort
 from orchestrator.utils.helpers import to_camel
 
 PRODUCT_SORT_FUNCTIONS_BY_COLUMN = {
-    to_camel(key): generic_column_sort(value) for [key, value] in inspect(ProductTable).columns.items()
+    to_camel(key): generic_column_sort(value, ProductTable) for [key, value] in inspect(ProductTable).columns.items()
 }
 
 product_sort_fields = list(PRODUCT_SORT_FUNCTIONS_BY_COLUMN.keys())
