10903d641676e5e2ffbd77280cc57eddde5d30b7
chriscummings <chriscummings@es.net>
2024-03-07 16:26:59 +0100

    ci: fix noqa comments even more

--- orchestrator/cli/generate.py
+++ orchestrator/cli/generate.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 SURF.
+# Copyright 2019-2020 SURF, ESnet.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,6 +10,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# ruff: noqa: S603
 import subprocess
 from pathlib import Path
 
