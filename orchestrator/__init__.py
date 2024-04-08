8d6d49824eb3c9701f10a4c89e61143eeded1087
Daniel Marjenburgh <daniel.marjenburgh@surf.nl>
2024-04-08 15:20:01 +0200

    Bump version to 2.2.1 (#607)

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings, oauth2_settings
