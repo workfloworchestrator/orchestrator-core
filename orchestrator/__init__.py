04e7992981a827e263a72f4e56932c9f830119b5
Mark90 <mark_moes@live.nl>
2024-06-20 10:28:59 +0200

    Bump version to 2.3.0

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "2.3.0rc6"
+__version__ = "2.3.0"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings
