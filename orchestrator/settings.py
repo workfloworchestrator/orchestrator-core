a561690128be1e9546f28caa77038aa5f2e4c3ab
Richie B2B <github@vdberg.org>
2024-07-02 11:03:15 +0200

    Bump psycopg to 3.2.1 (#696)
    * Bump psycopg to 3.2.1
    
    * Convert psycopg2 connection strings to psycopg v3 format
    
    ---------
    
    Co-authored-by: Peter Boers <peter.boers@surf.nl>

--- orchestrator/settings.py
+++ orchestrator/settings.py
@@ -14,12 +14,17 @@
 import secrets
 import string
 from pathlib import Path
+import warnings
 
 from pydantic import PostgresDsn, RedisDsn
 from pydantic_settings import BaseSettings
 
 from oauth2_lib.settings import oauth2lib_settings
 from orchestrator.types import strEnum
+
+
+class OrchestratorDeprecationWarning(DeprecationWarning):
+    pass
 
 
 class ExecutorType(strEnum):
@@ -48,7 +53,7 @@ class AppSettings(BaseSettings):
     EXECUTOR: str = ExecutorType.THREADPOOL
     WORKFLOWS_SWAGGER_HOST: str = "localhost"
     WORKFLOWS_GUI_URI: str = "http://localhost:3000"
-    DATABASE_URI: PostgresDsn = "postgresql://nwa:nwa@localhost/orchestrator-core"  # type: ignore
+    DATABASE_URI: PostgresDsn = "postgresql+psycopg://nwa:nwa@localhost/orchestrator-core"  # type: ignore
     MAX_WORKERS: int = 5
     MAIL_SERVER: str = "localhost"
     MAIL_PORT: int = 25
@@ -78,6 +83,21 @@ class AppSettings(BaseSettings):
     DEFAULT_CUSTOMER_SHORTCODE: str = "default-cust"
     DEFAULT_CUSTOMER_IDENTIFIER: str = "59289a57-70fb-4ff5-9c93-10fe67b12434"
     TASK_LOG_RETENTION_DAYS: int = 3
+
+    def __init__(self):
+        super(AppSettings, self).__init__()
+        self.DATABASE_URI = PostgresDsn(convert_database_uri(str(self.DATABASE_URI)))
+
+
+def convert_database_uri(db_uri: str) -> str:
+    if db_uri.startswith(("postgresql://", "postgresql+psycopg2://")):
+        db_uri = "postgresql+psycopg" + db_uri[db_uri.find("://") :]
+        warnings.filterwarnings("always", category=OrchestratorDeprecationWarning)
+        warnings.warn(
+            "DATABASE_URI converted to postgresql+psycopg:// format, please update your enviroment variable",
+            OrchestratorDeprecationWarning,
+        )
+    return db_uri
 
 
 app_settings = AppSettings()
