f86208b1d4beb1e0a7db162f5faafa88dbece1e0
Peter Boers <peter.boers@surf.nl>
2024-07-10 09:48:58 +0200

    Revert "Bump psycopg to 3.2.1 (#696)"
    This reverts commit a5616901

--- test/unit_tests/conftest.py
+++ test/unit_tests/conftest.py
@@ -167,7 +167,7 @@ def db_uri(worker_id):
         Database uri to be used in the test thread
 
     """
-    database_uri = os.environ.get("DATABASE_URI", "postgresql+psycopg://nwa:nwa@localhost/orchestrator-core-test")
+    database_uri = os.environ.get("DATABASE_URI", "postgresql://nwa:nwa@localhost/orchestrator-core-test")
     if worker_id == "master":
         # pytest is being run without any workers
         return database_uri
@@ -198,9 +198,9 @@ def database(db_uri):
         url.database = "postgres"
     engine = create_engine(url)
     with closing(engine.connect()) as conn:
-        conn.commit()
-        conn.execution_options(isolation_level="AUTOCOMMIT").execute(text(f'DROP DATABASE IF EXISTS "{db_to_create}";'))
-        conn.commit()
+        conn.execute(text("COMMIT;"))
+        conn.execute(text(f'DROP DATABASE IF EXISTS "{db_to_create}";'))
+        conn.execute(text("COMMIT;"))
         conn.execute(text(f'CREATE DATABASE "{db_to_create}";'))
 
     run_migrations(db_uri)
@@ -211,10 +211,8 @@ def database(db_uri):
     finally:
         db.wrapped_database.engine.dispose()
         with closing(engine.connect()) as conn:
-            conn.commit()
-            conn.execution_options(isolation_level="AUTOCOMMIT").execute(
-                text(f'DROP DATABASE IF EXISTS "{db_to_create}";')
-            )
+            conn.execute(text("COMMIT;"))
+            conn.execute(text(f'DROP DATABASE IF EXISTS "{db_to_create}";'))
 
 
 @pytest.fixture(autouse=True)
