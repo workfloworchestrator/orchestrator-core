a8a2ba07e1641db12e1733b557cfbd7630061b16
Mark90 <mark_moes@live.nl>
2024-07-18 11:41:25 +0200

    #351 In graphql types, represent relations as resolvers instead of lists to prevent n+1 queries
    Signed-off-by: Mark90 <mark_moes@live.nl>

--- orchestrator/graphql/schemas/workflow.py
+++ orchestrator/graphql/schemas/workflow.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING, Annotated
 import strawberry
 
 from orchestrator.config.assignee import Assignee
+from orchestrator.db import WorkflowTable
+from orchestrator.graphql.schemas.helpers import get_original_model
 from orchestrator.schemas import StepSchema, WorkflowSchema
 from orchestrator.workflows import get_workflow
 
@@ -21,7 +23,9 @@ class Workflow:
     async def products(self) -> list[Annotated["ProductType", strawberry.lazy(".product")]]:
         from orchestrator.graphql.schemas.product import ProductType
 
-        return [ProductType.from_pydantic(product) for product in self._original_model.products]  # type: ignore
+        model = get_original_model(self, WorkflowTable)
+
+        return [ProductType.from_pydantic(product) for product in model.products]
 
     @strawberry.field(description="Return all steps for this workflow")  # type: ignore
     def steps(self) -> list[Step]:
