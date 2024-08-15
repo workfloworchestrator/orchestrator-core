0be79835f857e5671342b4d445338c9728a53048
Mark90 <mark_moes@live.nl>
2024-08-15 17:58:37 +0200

    Retry connection/timeout errors for client in RedisBroadcast
    Signed-off-by: Mark90 <mark_moes@live.nl>

--- orchestrator/utils/redis.py
+++ orchestrator/utils/redis.py
@@ -17,10 +17,13 @@ from os import getenv
 from typing import Any, Callable
 from uuid import UUID
 
+import redis.exceptions
 from anyio import CancelScope, get_cancelled_exc_class
 from redis import Redis
 from redis.asyncio import Redis as AIORedis
 from redis.asyncio.client import Pipeline, PubSub
+from redis.asyncio.retry import Retry
+from redis.backoff import EqualJitterBackoff
 from structlog import get_logger
 
 from orchestrator.services.subscriptions import _generate_etag
@@ -134,7 +137,12 @@ class RedisBroadcast:
 
     def __init__(self, redis_url: str):
         # self.client = AIORedis.from_url(redis_url, socket_keepalive=True, health_check_interval=20)
-        self.client = AIORedis.from_url(redis_url)
+        self.client = AIORedis.from_url(
+            redis_url,
+            retry_on_error=[redis.exceptions.ConnectionError],
+            retry_on_timeout=True,
+            retry=Retry(EqualJitterBackoff(base=0.05), 2),
+        )
         self.redis_url = redis_url
 
     @asynccontextmanager
