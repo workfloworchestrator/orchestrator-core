4e60b54b8c800218a71414d536b1680be79e057e
Igor van Spengen <igor.van.spengen@outlook.com>
2024-10-17 15:54:19 +0200

    tmp: Fix lib import and unit test

--- orchestrator/security.py
+++ orchestrator/security.py
@@ -34,7 +34,7 @@ oauth_client_credentials.register(
 
 
 async def authenticate(
-    request: Request, token: Annotated[str | None, Depends(HttpBearerExtractor())] = None
+    request: Request, token: Annotated[str | None, Depends(HttpBearerExtractor())] = None # type:ignore
 ) -> OIDCUserModel | None:
     return await request.app.auth_manager.authentication.authenticate(request, token)
 
