540613e02c98d5e777a6529876045850b776b704
Mark90 <mark_moes@live.nl>
2024-10-30 14:23:06 +0100

    Bump version to 2.8.0

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "2.8.0rc5"
+__version__ = "2.8.0"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings
