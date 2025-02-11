ae8121bfaceb6fbf9883b9cc236cf8c71c897f53
Sparrow1029 <19390913+Sparrow1029@users.noreply.github.com>
2025-02-11 15:58:47 -0800

    Fix version endpoint

--- orchestrator/graphql/resolvers/version.py
+++ orchestrator/graphql/resolvers/version.py
@@ -1,24 +1,21 @@
-
 from structlog import get_logger
 
-from orchestrator.graphql.schemas.version import VersionType
+from orchestrator.graphql.schemas.version import VERSIONS, VersionType
 from orchestrator.graphql.types import OrchestratorInfo
 from orchestrator.graphql.utils import create_resolver_error_handler
 
 logger = get_logger(__name__)
 
 
-def resolve_version(info: OrchestratorInfo) -> VersionType:
+def resolve_version(info: OrchestratorInfo) -> VersionType | None:
     logger.debug("resolve_version() called")
     _error_handler = create_resolver_error_handler(info)
 
     ver = None
     try:
-        ver = VersionType()
+        ver = VersionType(application_versions=VERSIONS)
     except Exception as e:
         logger.error(f"Error getting version: {str(e)}")
-
-    if not ver:
         _error_handler("Failed to retrieve orchestrator_core version", extensions={"code": "PACKAGE_VERSION_ERROR"})
 
-    return VersionType(orchestrator_core=ver)
+    return ver
--- orchestrator/graphql/schemas/version.py
+++ orchestrator/graphql/schemas/version.py
@@ -1,12 +1,10 @@
 import strawberry
-from pydantic import Field
 
 from orchestrator import __version__
-
 
 VERSIONS = [f"orchestrator-core: {__version__}"]
 
 
 @strawberry.type
 class VersionType:
-    application_versions: list[str] = Field(default=VERSIONS)
+    application_versions: list[str]
