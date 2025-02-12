6ef35ebd225733f24098f60d58b06947594aa80e
gstewart <gstewart@es.net>
2025-02-12 13:58:08 -0800

    update test

--- test/unit_tests/graphql/test_version.py
+++ test/unit_tests/graphql/test_version.py
@@ -10,17 +10,12 @@ def get_workflows_query() -> bytes:
         }
         }
     """
-    return json.dumps(
-        {
-            "operationName": "VersionQuery",
-            "query": query
-        }
-    ).encode("utf-8")
+    return json.dumps({"operationName": "VersionQuery", "query": query}).encode("utf-8")
 
 
 def test_workflows_query(test_client):
-    data = get_workflows_query(first=2)
-    response = test_client.get("/api/graphql", content=data, headers={"Content-Type": "application/json"})
-    assert HTTPStatus.OK == response.status_code
+    data = get_workflows_query()
+    response = test_client.post("/api/graphql", content=data, headers={"Content-Type": "application/json"})
+    assert response.status_code == HTTPStatus.OK
     result = response.json()
     assert "errors" not in result
