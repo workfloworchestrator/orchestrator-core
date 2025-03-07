991dc0b60835bc6d3f138ed6e45059e5b61841f8
Mark90 <mark_moes@live.nl>
2025-03-07 10:30:06 +0100

    WIP

---
+++ orchestrator/migrations/versions/schema/2025-03-06_42b3d076a85b_add_function.sql
@@ -0,0 +1,41 @@
+-- TODO rename functions to something better
+
+CREATE OR REPLACE FUNCTION get_subscription_instance_fields(sub_inst_id uuid)
+    RETURNS jsonb
+    LANGUAGE sql
+    STABLE PARALLEL SAFE AS
+$func$
+select jsonb_object_agg(rts.key, rts.val)
+from (select attr.key,
+             attr.val
+      from subscription_instances si
+               join product_blocks pb ON si.product_block_id = pb.product_block_id
+               cross join lateral (
+          values ('subscription_instance_id', si.subscription_instance_id::text),
+                 ('owner_subscription_id', si.subscription_id::text),
+                 ('name', pb.name)
+          ) as attr(key, val)
+      where si.subscription_instance_id = sub_inst_id
+      union
+      select rt.resource_type key,
+             siv.value        val
+      from subscription_instance_values siv
+               join resource_types rt on siv.resource_type_id = rt.resource_type_id
+      where siv.subscription_instance_id = sub_inst_id) as rts
+$func$;
+
+
+
+CREATE OR REPLACE FUNCTION get_subscription_instance(sub_inst_id uuid)
+    RETURNS jsonb
+    LANGUAGE sql
+    STABLE PARALLEL SAFE AS
+$func$
+select get_subscription_instance_fields(sub_inst_id) ||
+       coalesce(jsonb_object_agg(depends_on.block_name, depends_on.block_instances), '{}'::jsonb)
+from (select sir.domain_model_attr                                       block_name,
+             jsonb_agg(get_subscription_instance(sir.depends_on_id)) as block_instances
+      from subscription_instance_relations sir
+      where sir.in_use_by_id = sub_inst_id
+      group by block_name) as depends_on
+$func$;
