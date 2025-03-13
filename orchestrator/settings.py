7fa47e2c33286ba5b9cdec624bf666c17b3f0b47
Mark90 <mark_moes@live.nl>
2025-03-13 13:53:55 +0100

    Also use ENABLE_SUBSCRIPTION_MODEL_OPTIMIZATIONS to toggle subscription model caching

--- orchestrator/settings.py
+++ orchestrator/settings.py
@@ -92,7 +92,9 @@ class AppSettings(BaseSettings):
     ENABLE_GRAPHQL_STATS_EXTENSION: bool = False
     VALIDATE_OUT_OF_SYNC_SUBSCRIPTIONS: bool = False
     FILTER_BY_MODE: Literal["partial", "exact"] = "exact"
-    ENABLE_SUBSCRIPTION_MODEL_OPTIMIZATIONS: bool = True  # TODO remove feature toggle before merging
+    ENABLE_SUBSCRIPTION_MODEL_OPTIMIZATIONS: bool = (
+        True  # True=ignore cache + optimized DB queries; False=use cache + unoptimized DB queries
+    )
 
     def __init__(self) -> None:
         super(AppSettings, self).__init__()
