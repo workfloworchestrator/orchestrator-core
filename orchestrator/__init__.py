882355373da323690df82cdaf5be6c680ca35091
Peter Boers <peter.boers@surf.nl>
2025-03-20 11:22:06 +0100

    Downgrade Pydantic

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "3.1.2rc2"
+__version__ = "3.1.2rc3"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings
