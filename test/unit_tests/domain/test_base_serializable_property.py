173377d78ad6b7200456c3ecd8451a3539b306b6
Mark90 <mark_moes@live.nl>
2025-03-28 15:32:08 +0100

    Add testcase for retrieving SubscriptionModel in serializable property

--- test/unit_tests/domain/test_base_serializable_property.py
+++ test/unit_tests/domain/test_base_serializable_property.py
@@ -1,5 +1,8 @@
+from unittest import mock
+
 from pydantic import computed_field
 
+from orchestrator.domain import SubscriptionModel
 from orchestrator.domain.base import DomainModel
 
 
@@ -67,3 +70,83 @@ def test_nested_serializable_property():
     model = ParentDomainModel(derived=DerivedDomainModel(int_field=13))
 
     assert model.model_dump() == {"derived": {"int_field": 13, "double_int_field": 26}}
+
+
+def test_subscription_model_in_serializable_property(
+    generic_subscription_1, generic_subscription_2, generic_product_type_1, generic_product_type_2, monitor_sqlalchemy
+):
+    """Ensure that serializable properties can retrieve other SubscriptionModels without duplicate queries.
+
+    While this works and will continue to work, is not recommended practice because reconstructing the entire
+    subscription only to retrieve a few attributes is "overkill" and impacts peformance.
+    Issue #899 has been created to track the design of a more efficient implementation.
+    """
+    _, GenericProductOne = generic_product_type_1
+    _, GenericProductTwo = generic_product_type_2
+
+    # One typical usecase -at SURF- is to define a computed 'title' for a product block based on details from
+    # the owner subscription of a 'foreign' product block.
+    #
+    # For example:
+    #
+    # class PeerBlockProvisioning(PeerBlockInactive, lifecycle=[SubscriptionLifecycle.PROVISIONING]):
+    #     port: IpPeerPortBlock
+    #
+    #     @computed_field
+    #     @property
+    #     def title(self) -> str:
+    #         sap = self.port.sap
+    #         subscription = SubscriptionModel.from_subscription(sap.owner_subscription_id)
+    #         return f"IP Peering on {subscription.description} vlan {sap.vlanrange}"
+    #
+    # Below we test a simplified but technically identical usecase.
+
+    # given
+
+    class DerivedDomainModel(DomainModel):
+        @computed_field  # type: ignore[misc]
+        @property
+        def title(self) -> int:
+            subscription1 = GenericProductOne.from_subscription(generic_subscription_1)
+            subscription2 = GenericProductTwo.from_subscription(generic_subscription_2)
+            return f"{subscription1.description} {subscription1.pb_2.rt_2} - {subscription2.description} {subscription2.pb_3.rt_2}"
+
+        @computed_field
+        @property
+        def foobar(self) -> int:
+            subscription1 = GenericProductOne.from_subscription(generic_subscription_1)
+            return f"{subscription1.description} {subscription1.pb_2.rt_2}"
+
+        int_field: int
+
+    model = DerivedDomainModel(int_field=123)
+
+    # when
+
+    # Spy on the _get_subscription method to observe the number of queries
+    with mock.patch.object(
+        SubscriptionModel,
+        "_get_subscription",
+        spec=SubscriptionModel._get_subscription,
+        side_effect=SubscriptionModel._get_subscription,
+    ) as mock_get_subscription:
+        actual_result = model.model_dump()
+
+    # then
+
+    expected_result = {
+        "int_field": 123,
+        "title": "Generic Subscription One 42 - Generic Subscription Two 42",
+        "foobar": "Generic Subscription One 42",
+    }
+    assert actual_result == expected_result
+
+    actual_calls = sorted(mock_get_subscription.mock_calls)
+    expected_calls = sorted(
+        [
+            mock.call(generic_subscription_1),
+            mock.call(generic_subscription_1),
+            mock.call(generic_subscription_2),
+        ]
+    )
+    assert actual_calls == expected_calls
