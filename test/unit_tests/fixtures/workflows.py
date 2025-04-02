1b76824f85c491ff96cf6464e5b75561d385b147
Wouter Huisman <wouter.huisman@surf.nl>
2025-04-02 16:57:25 +0200

    fix mypy

--- test/unit_tests/fixtures/workflows.py
+++ test/unit_tests/fixtures/workflows.py
@@ -9,7 +9,14 @@ from orchestrator.utils.datetime import nowtz
 def add_soft_deleted_workflows():
     def _add_soft_deleted_workflow(n: int):
         for i in range(n):
-            db.session.add(WorkflowTable(name=f"deleted_workflow_{i}", description="deleted workflow", target=Target.SYSTEM, deleted_at=nowtz()))
+            db.session.add(
+                WorkflowTable(
+                    name=f"deleted_workflow_{i}",
+                    description="deleted workflow",
+                    target=Target.SYSTEM,
+                    deleted_at=nowtz(),
+                )
+            )
         db.session.commit()
 
     return _add_soft_deleted_workflow
