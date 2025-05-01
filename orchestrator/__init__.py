e7f5c1b58ab2bf0a0c6fb281e5a838c5a67432b6
Igor van Spengen <igor.van.spengen@outlook.com>
2025-05-01 15:53:20 +0200

    bump to 4.0.0rc2

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -13,7 +13,7 @@
 
 """This is the orchestrator workflow engine."""
 
-__version__ = "4.0.0rc"
+__version__ = "4.0.0rc2"
 
 from orchestrator.app import OrchestratorCore
 from orchestrator.settings import app_settings
