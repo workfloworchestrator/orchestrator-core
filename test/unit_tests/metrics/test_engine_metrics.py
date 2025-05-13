8804b7b72cd5a2568ba0842756b7d9af888a67a4
Karel van Klink <karel.vanklink@geant.org>
2025-05-13 13:21:15 +0200

    Fix metrics unit test
    Fix unit test false failing if correct results appear in a different order

--- test/unit_tests/metrics/test_engine_metrics.py
+++ test/unit_tests/metrics/test_engine_metrics.py
@@ -4,13 +4,14 @@ from test.unit_tests.metrics.conftest import EMPTY_METRICS
 def test_engine_metrics_success(test_client) -> None:
     test_client.put("/api/settings/status", json={"global_lock": True})
     response = test_client.get("api/metrics")
-    expected_metric = """# HELP wfo_engine_status Current workflow engine status.
-# TYPE wfo_engine_status gauge
-wfo_engine_status{wfo_engine_status="PAUSED"} 1.0
-wfo_engine_status{wfo_engine_status="PAUSING"} 0.0
-wfo_engine_status{wfo_engine_status="RUNNING"} 0.0
-"""
-    assert expected_metric in response.text
+    expected_metric_lines = [
+        "# HELP wfo_engine_status Current workflow engine status.",
+        "# TYPE wfo_engine_status gauge",
+        'wfo_engine_status{wfo_engine_status="PAUSED"} 1.0',
+        'wfo_engine_status{wfo_engine_status="PAUSING"} 0.0',
+        'wfo_engine_status{wfo_engine_status="RUNNING"} 0.0',
+    ]
+    assert all(line in response.text for line in expected_metric_lines)
 
     test_client.put("/api/settings/status", json={"global_lock": False})
     response = test_client.get("/api/metrics")
