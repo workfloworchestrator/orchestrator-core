dbf4288f11aa4a138b5118a357bd856a17bb945d
Igor van Spengen <igor.van.spengen@outlook.com>
2025-06-02 16:31:54 +0200

    Fix linting'

--- test/unit_tests/db/test_migration_does_column_exist.py
+++ test/unit_tests/db/test_migration_does_column_exist.py
@@ -1,7 +1,7 @@
-from orchestrator.db import db
-from sqlalchemy import text
 
+from orchestrator.db import db
 from orchestrator.migrations.helpers import has_table_column
+
 
 def test_select_from_table():
     # Since your fixture does not yield a session but manages scoped_session internally,
