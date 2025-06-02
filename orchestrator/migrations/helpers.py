7b7ccc7b1fa31d587509df37106141483ae5f582
Igor van Spengen <Igoranze@users.noreply.github.com>
2025-06-02 09:01:16 +0200

    957 Check for table (#960)
    * Remove the trailing \
    
    * Add debuggin
    
    * fix when table does not exist
    
    * fix linting
    
    * Update to 4.0.3
    
    * add details to function

--- orchestrator/migrations/helpers.py
+++ orchestrator/migrations/helpers.py
@@ -27,14 +27,22 @@ logger = structlog.get_logger(__name__)
 def has_table_column(table_name: str, column_name: str, conn: sa.engine.Connection) -> bool:
     """Checks if the specified column exists in a given table.
 
+    inspector.get_columns raises an exception if the table does not exist, so we catch that exception and return False.
+    This is useful for migrations where you want to ensure that a column exists before performing operations on it.
+
     :param table_name: Name of the database table
     :param column_name: Name of the column to check
     :param conn: SQLAlchemy database Connection
-    :return: True if column exists, False otherwise
+    :return: True if the column exists, False otherwise
     """
     inspector = sa.inspect(conn.engine)
-    columns = inspector.get_columns(table_name)
-    return any(col["name"] == column_name for col in columns)
+    try:
+        columns = inspector.get_columns(table_name)
+        return any(col["name"] == column_name for col in columns)
+    except sa.exc.NoSuchTableError:
+        # On some migrations the table might not exist yet, so we catch the exception
+        logger.warning(f"Table {table_name} does not exist.")
+        return False
 
 
 def get_resource_type_id_by_name(conn: sa.engine.Connection, name: str) -> UUID:
