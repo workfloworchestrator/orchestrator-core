dfedb61412104d97a29080105ebc074e537d5321
Igor van Spengen <Igoranze@users.noreply.github.com>
2025-06-02 16:54:10 +0200

    Update test_migration_does_column_exist.py

--- test/unit_tests/db/test_migration_does_column_exist.py
+++ test/unit_tests/db/test_migration_does_column_exist.py
@@ -4,8 +4,8 @@ from orchestrator.migrations.helpers import has_table_column
 
 
 def test_select_from_table():
-    # Since your fixture does not yield a session but manages scoped_session internally,
-    # we access the session via your db.wrapped_database.scoped_session
+    # Testing if Table Workflows exist with column is_task 
+    # it should because the db.session depends on the session where all migrations are already run
     session = db.session
     result = has_table_column(table_name="workflows", column_name="is_task", conn=session)
     assert result is True, "Column 'is_task' does not exist in 'workflows' table"
