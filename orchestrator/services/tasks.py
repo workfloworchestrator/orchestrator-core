6aec32a9a09f29d492de9d630fe22b99fa9d3bb8
Igor van Spengen <igor.van.spengen@outlook.com>
2026-03-18 16:16:59 +0100

    revert len bug

--- orchestrator/services/tasks.py
+++ orchestrator/services/tasks.py
@@ -123,14 +123,10 @@ class CeleryJobWorkerStatus(WorkerStatus):
 
         inspection: Inspect = _celery.control.inspect()
         stats = inspection.stats()
-        scheduled = inspection.scheduled()
-        reserved = inspection.reserved()
-        active = inspection.active()
+        self.number_of_workers_online = len(stats)
 
-        self.number_of_workers_online = len(stats) if stats else 0
-
-        def sum_items(d: dict | None) -> int:
+        def sum_items(d: dict) -> int:
             return sum(len(lines) for _, lines in d.items()) if d else 0
 
-        self.number_of_queued_jobs = sum_items(scheduled) + sum_items(reserved)
-        self.number_of_running_jobs = sum(len(tasks) for w, tasks in active.items()) if active else 0
+        self.number_of_queued_jobs = sum_items(inspection.scheduled()) + sum_items(inspection.reserved())
+        self.number_of_running_jobs = sum(len(tasks) for w, tasks in inspection.active().items())
